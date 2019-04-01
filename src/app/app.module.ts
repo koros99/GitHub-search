9998737ea09247b20e90166d053631cf449edc08
koros99 <koronje254@gmail.com>
2019-04-01 08:35:30 +0300

    Add components and providers for routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-// import { HttpModule } from '@angular/http';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { ProfileRequestService } from './profile-http/profile-request.service';
@@ -19,7 +18,6 @@ import { ProfileComponent } from './profile/profile.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    // HttpModule,
     HttpClientModule,
     FormsModule
   ],
