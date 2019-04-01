35805ddfb8093e53084fe0ffa74d6fcd1bfc8ed1
koros99 <koronje254@gmail.com>
2019-04-01 08:38:05 +0300

    Add promise function to consume API

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -27,45 +27,45 @@ export class ProfileComponent implements OnInit {
     this.profileRequestService.getProfile();
     this.profile = this.profileRequestService.profile;
   }
-  // getProfile(){
-  //   interface ApiResponse{
-  //     login: string;
-  //     name: string;
-  //     bio: any;
-  //     public_repos: number;
-  //     followers: number;
-  //     following: number;
-  //     avatar_url: any;
-  //     html_url:any;
-  //   }
-  //   let promise = new Promise((resolve,reject)=>{
-  //     this.http.get<ApiResponse>(environment.apiUrl + this.username + "?access_token" + environment.access_token).toPromise().then(response=>{
-  //       this.profile.avatar = response.avatar_url
-  //       this.profile.username = response.login
-  //       this.profile.name = response.name
-  //       this.profile.bio = response.bio
-  //       this.profile.repos = response.public_repos
-  //       this.profile.followers = response.followers
-  //       this.profile.following = response.following
-  //       this.profile.link = response.html_url
-  //
-  //       resolve();
-  //     },
-  //     error=>{
-  //       this.profile.avatar = ""
-  //       this.profile.username = "koros99"
-  //       this.profile.name = "Victor Kilel"
-  //       this.profile.bio = "Tech Developer"
-  //       this.profile.repos = 19
-  //       this.profile.followers = 0
-  //       this.profile.following = 1
-  //       this.profile.link = "https://github.com/koros99"
-  //
-  //       reject(error);
-  //     })
-  //   })
-  //   return promise
-  // }
+  getProfile(){
+    interface ApiResponse{
+      login: string;
+      name: string;
+      bio: any;
+      public_repos: number;
+      followers: number;
+      following: number;
+      avatar_url: any;
+      html_url:any;
+    }
+    let promise = new Promise((resolve,reject)=>{
+      this.http.get<ApiResponse>(environment.apiUrl + this.username + "?access_token" + environment.access_token).toPromise().then(response=>{
+        this.profile.avatar = response.avatar_url
+        this.profile.username = response.login
+        this.profile.name = response.name
+        this.profile.bio = response.bio
+        this.profile.repos = response.public_repos
+        this.profile.followers = response.followers
+        this.profile.following = response.following
+        this.profile.link = response.html_url
+
+        resolve();
+      },
+      error=>{
+        this.profile.avatar = ""
+        this.profile.username = "koros99"
+        this.profile.name = "Victor Kilel"
+        this.profile.bio = "Tech Developer"
+        this.profile.repos = 19
+        this.profile.followers = 0
+        this.profile.following = 1
+        this.profile.link = "https://github.com/koros99"
+
+        reject(error);
+      })
+    })
+    return promise
+  }
   ngOnInit() {
     this.profile.avatar = "https://avatars1.githubusercontent.com/u/47349082?v=4"
     this.profile.username = "koros99"
