cdfa7ff040d1be31725a4f4a90236846309eb826
koros99 <koronje254@gmail.com>
2019-04-02 16:17:08 +0300

    Add class for defining repositories

---
+++ src/app/repos-class/repos.ts
@@ -0,0 +1,10 @@
+export class Repos {
+  constructor(
+    public name: string,
+    public url: string,
+    public forks: number,
+    public description: string,
+    public stars: number,
+    public language: string,
+  ){}
+}
