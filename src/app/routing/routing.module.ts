d83b66c2c5b80f9abcee7c63cc8a3aa8772e4df9
koros99 <koronje254@gmail.com>
2019-04-02 16:22:00 +0300

    Add paths for components

--- src/app/routing/routing.module.ts
+++ src/app/routing/routing.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 
-import { ProfileComponent } from '../profile/profile.component';
+import { ProfileComponent }  from '../profile/profile.component';
+import { NotFoundComponent } from '../not-found/not-found.component';
+import { ReposComponent } from '../repos/repos.component';
 
 const routes: Routes = [
-  {path: 'profile', component: ProfileComponent}
+  {path: "profile", component: ProfileComponent},
+  {path: "repos", component: ReposComponent},
+  {path:"", redirectTo:"/profile",pathMatch:"full"},
+  {path:"**", component:NotFoundComponent}
 ]
 
 @NgModule({
