cb9a77c24336f3ef78836ac2c5d375039922ec56
koros99 <koronje254@gmail.com>
2019-04-02 16:25:57 +0300

    Fix function names errors and other typos

--- src/app/profile-http/profile-request.service.ts
+++ src/app/profile-http/profile-request.service.ts
@@ -1,60 +1,131 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Profile } from '../profile-class/profile';
+import { Repos } from '../repos-class/repos';
 import { environment } from '../../environments/environment';
 
+interface ReposInterface {
+  total_count: number;
+  incomplete_results: boolean;
+  items: Repo[];
+}
+
+interface Repo {
+  name: string;
+  html_url: string;
+  forks: number;
+  description: string;
+  stars_number: number;
+  language: string;
+  //full_name: string;
+}
 @Injectable()
 export class ProfileRequestService {
+  access_token: string = environment.access_token;
+  profileUrl: string = environment.apiUrl;
+  reposUrl: string = environment.reposUrl;
+
   profile: Profile;
-  username: string;
+  profileRepos: Repos[];
+  searchedRepos: Repos[];
 
-  constructor(private http:HttpClient) {
-    this.profile = new Profile ("", "", "", "", "", "", "", "")
+  constructor(private http: HttpClient) {
+    this.profileRepos = [];
+    this.searchedRepos = [];
+
   }
 
-  newProfile(username: string){
-    this.username = username;
-  }
 
-
-  getProfile(){
-    interface ApiResponse{
+  getProfile(username: string) {
+    interface ApiResponse {
+      avatar_url: any;
       login: string;
       name: string;
       bio: any;
       public_repos: number;
       followers: number;
       following: number;
-      avatar_url: any;
-      html_url:any;
+      html_url: any;
     }
-    let promise = new Promise((resolve,reject)=>{
-      this.http.get<ApiResponse>(environment.apiUrl + this.username + "?access_token" + environment.access_token).toPromise().then(response=>{
-        this.profile.avatar = response.avatar_url
-        this.profile.username = response.login
-        this.profile.name = response.name
-        this.profile.bio = response.bio
-        this.profile.repos = response.public_repos
-        this.profile.followers = response.followers
-        this.profile.following = response.following
-        this.profile.link = response.html_url
-
+    let promise = new Promise((resolve, reject) => {
+      let accessUrl = this.profileUrl + username + "?access_token=" + this.access_token;
+      this.http.get<ApiResponse>(accessUrl).toPromise().then(response => {
+        this.profile = new Profile(
+          response.avatar_url,
+          response.login,
+          response.name,
+          response.bio,
+          response.public_repos,
+          response.followers,
+          response.following,
+          response.html_url
+        )
         resolve();
       },
-      error=>{
-        this.profile.avatar = ""
-        this.profile.username = "koros99"
-        this.profile.name = "Victor Kilel"
-        this.profile.bio = "Tech Developer"
-        this.profile.repos = 19
-        this.profile.followers = 0
-        this.profile.following = 1
-
-        reject(error);
-      })
+        error => {
+          reject(error);
+        })
     })
 
     return promise;
   }
+  getProfileRepos(username: string) {
+    let promise = new Promise((resolve, reject) => {
+      let profileReposUrl = this.profileUrl + username + "/repos?access_token=" + this.access_token;
+      this.http.get<Repo[]>(profileReposUrl).toPromise().then(response => {
+        this.profileRepos = [];
+        if (response.length > 0) {
+          for (var i = 0; i < response.length; i++) {
+            this.profileRepos.push(
+              new Repos(
+                response[i].name,
+                response[i].html_url,
+                response[i].forks,
+                response[i].description,
+                response[i].stars_number,
+                response[i].language
+              )
+            )
+          }
+        }
+        resolve()
+      },
+        error => {
+          this.profileRepos = [];
+          reject()
+        }
+      )
+    })
+    return promise;
 
+  }
+
+  searchRepos(query: string) {
+    let promise = new Promise((resolve, reject) => {
+      let searchReposUrl = this.reposUrl + query + "&access_token=" + this.access_token;
+      this.http.get<ReposInterface>(searchReposUrl).toPromise().then(response => {
+        this.searchedRepos = [];
+        if (response.total_count > 0) {
+          for (var i = 0; i < response.items.length; i++) {
+            this.searchedRepos.push(
+              new Repos(
+                response.items[i].name,
+                response.items[i].html_url,
+                response.items[i].forks,
+                response.items[i].description,
+                response.items[i].stars_number,
+                response.items[i].language,
+              )
+            )
+          }
+        }
+        resolve()
+      }, error => {
+        this.searchedRepos = [];
+        reject()
+      }
+      )
+    })
+    return promise;
+  }
 }
